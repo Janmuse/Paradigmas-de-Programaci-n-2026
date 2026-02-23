40b9dbda20ba096d7a736e826084ee344cf5a150
Janmuse <jorge.and005@gmail.com>
2026-02-22 23:48:39 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package org.example;
+public class Main {
+    static void main() {
+
+        Integer [] intVector = { 101, 99, 12, 19, 21, 111, 345, 25, 77, 81 };
+        Integer [] resultInt = Utils.ordena (intVector);
+        System.out.println("Con int");
+        Utils.imprimir(resultInt);
+
+        System.out.println("Con float");
+        Float [] floatVector = { 19.1f, 456.6f, 23.45f, 12.34f, 11.11f, 354.55f, 78.45f, 28.33f, 45.99f, 108.88f };
+        Float[] resultFloat = Utils.ordena (floatVector);
+        Utils.imprimir(resultFloat);
+
+        System.out.println("Con string");
+        String [] strVector = {"zz", "aa", "cc", "hh", "bb", "ee", "ll"};
+//Fails con String
+//      String [] resultStr = Utils.ordena (strVector);
+        Utils.imprimir(strVector);
+    }
+}
---
+++ Utils.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.awt.print.Printable;
+
+public class Utils {
+    public static <T> void imprimir(T[] vector) {
+    for (int i= 0; i < vector.length; i++){
+        System.out.print(vector[i] + " ");
+     }
+    System.out.println("");
+    }
+
+    public static <T extends Number & Comparable> T[] ordena(T[] vector) {
+        quickSort(vector, 0, vector.length - 1);
+        return vector;
+    }
+
+    private static <T extends Number & Comparable> int partition(T[] vector, int low, int high) {
+        T pivot = vector[high];
+        int j = low -1;
+        for (int i = low; i < high; i++) {
+            if (vector[i].compareTo(pivot) < 0) {
+                j++;
+                T temp = vector[i];
+                vector[i] = vector[j];
+                vector[j] = temp;
+            }
+        }
+        T newpivot = vector[high];
+        vector[high] = vector[j + 1];
+        vector[j + 1] = newpivot;
+        return j + 1;
+    }
+
+    private static <T extends Number & Comparable> void quickSort(T[] vector, int low, int high) {
+        if(low < high){
+            int pivotPos = partition(vector, low, high);
+            quickSort(vector, low, pivotPos - 1);
+            quickSort(vector, pivotPos + 1, high);
+        }
+    }
+}
